d02fde3fcb47b3003770e28e8e204da15715e719
Bineer0826 <152459151+Bineer0826@users.noreply.github.com>
2024-02-02 16:35:57 +0900

    Update 스택.java

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -4,6 +4,12 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.Stack;
 
+
+//push X: 정수 X를 스택에 넣는 연산이다.
+//pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//size: 스택에 들어있는 정수의 개수를 출력한다.
+//empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
+//top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,7 +19,7 @@ class Main {
 
         for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine(), " ");
-            switch(st.nextToken()){
+            switch(st.nextToken()){ // 해당 케이스대로 값을 출력하도록 스위치를 사용
                 case "push":
                     stack.push(Integer.parseInt(st.nextToken()));
                     break;
